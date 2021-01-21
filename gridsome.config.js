24168fb2ec9d7da973f411fc69b6d5cec73891f0
Ivan Salazar <ivangio.salazar@gmail.com>
2021-01-20 19:56:52 -0600

    add configuration for gitlab pages

--- gridsome.config.js
+++ gridsome.config.js
@@ -5,8 +5,9 @@
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
 module.exports = {
-  siteName: 'Gridsome Blog Starter',
-  siteDescription: 'A simple, hackable & minimalistic starter for Gridsome that uses Markdown for content.',
+  siteName: 'Iván Salazar Blog',
+  siteDescription: 'Dev Blog',
+  outputDir: 'public',
 
   templates: {
     Post: '/:title',
